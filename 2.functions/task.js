8e45b6ebb17fe6d7271a4072d909d76f4b22186a
Vladimir <kosmo2233@yandex.ru>
2022-02-06 00:09:38 +0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   2.functions/task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,70 @@
-// Задание 1
+"use strict";
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = 0;
+  let avg;
 
-  // Ваш код
+    for (let i = 0; i < arr.length; ++i) {
+      if (max < arr[i]) {
+        max = arr[i];
+      } else if (min > arr[i]) {
+        min = arr[i];
+      }
 
-  return { min: min, max: max, avg: avg };
+      sum += arr[i];
+    }
+    avg = sum/arr.length;
+
+  return { 
+    min: min, 
+    max: max, 
+    avg: +avg.toFixed(2) // + здесь для преобразования к числу
+    };
 }
+ console.log(getArrayParams([-99, 99, 10]));
 
 // Задание 2
-function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+function worker(arr) {            // nasadka myasorubki
+  let sum = 0;
+  for (let i = 0; i < arr.length; ++i) {
+      sum += arr[i];
+  }
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
+function makeWork(arrOfArr, worker) {
+  let max = 0;
+    for(let i = 0; i < arrOfArr.length; ++i) {
+      let result = worker(arrOfArr[i]);
+      if (result > max) {
+        max = result;
+      }
+    }
+    return max;
 }
+makeWork([[1, 2, 3, 4], [10, 20, -10, -20]], worker);
+
 
 // Задание 3
-function worker2(arr) {
-  // Ваш код
-}
+function worker2(arr) {       
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    let distinct;
+    for (let i = 0; i < arr.length; ++i) {
+      distinct = Math.abs(max-min);
+    }
+    return distinct;
+  }
+
+  function makeWork(arrOfArr, worker2) {
+    let max = 0;
+      for(let i = 0; i < arrOfArr.length; ++i) {
+        let result = worker2(arrOfArr[i]);
+        if (result > max) {
+          max = result;
+        }
+      }
+      return max;
+  }
+  makeWork([[1, 2, 3, 4], [10, 20, -10, -20]], worker2);

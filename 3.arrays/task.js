047c4258d4b1aae42e0694fbf2d746bdcc9ed130
Vladimir <kosmo2233@yandex.ru>
2022-02-09 00:14:08 +0300

    tasks solved

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,26 @@
+"use strict";
 function compareArrays(arr1, arr2) {
   let result;
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
-  // Ваш код
-
-  return result; // boolean
+  
+  arr1.every(function(element, index) {
+    result = element === arr2[index]
+  });
+  return result;
 }
+  
+  // console.log(compareArrays([8,1,2], [8,1,2]));
+  
+
 
 function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+  let resultArr = arr.filter((item) => item > 0)
+                     .filter((item) => item %3  === 0)
+                     .map((item) => (item * 10));
 
   return resultArr; // array
 }

2eb800b2ddd4db76edb96efbcab1225ed631bf7a
Vladimir <kosmo2233@yandex.ru>
2022-02-23 23:43:19 +0300

    initial commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,7 +1,22 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+  let cache = [];
 
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let index = cache.findIndex((item) => item.hash == hash);
+    if (index !== -1) {
+      return `Из кэша: ${cache.result}`;
+    }
+
+    const result = func.call(this, ...args);
+    if (index.length > 5) {
+      index = index.shift();
+    }
+    cache.push({hash, result});
+    return `Вычисляем: ${result}`;
+  }
+  return wrapper;
+}
 
 function debounceDecoratorNew(func) {
   // Ваш код
